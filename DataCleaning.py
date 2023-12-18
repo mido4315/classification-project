e76b9b8195e9b53c1f25a63f85e996d0147f076f
Mohamed Mahmoud <96996503+mido4315@users.noreply.github.com>
2023-12-18 02:30:41 +0200

    Update DataCleaning.py

--- DataCleaning.py
+++ DataCleaning.py
@@ -1,4 +1,4 @@
-Q1 = df["failures"].quantile(0.25)
-Q3 = df["failures"].quantile(0.75)
+Q1 = df["Cholesterol"].quantile(0.25)
+Q3 = df["Heart Rate"].quantile(0.75)
 IQR = Q3 - Q1
-df = df[~((df["failures"] < (Q1 - 1.5 * IQR)) | (df["failures"] > (Q3 + 1.5 * IQR)))]
+df = df[~((df["Cholesterol"] < (Q1 - 1.5 * IQR)) | (df["Heart Rate"] > (Q3 + 1.5 * IQR)))]
